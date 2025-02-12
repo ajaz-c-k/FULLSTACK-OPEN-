072dd518b95f5b182a81fc093902a79c46d407bd
ajaz-c-k <157777100+ajaz-c-k@users.noreply.github.com>
2025-02-12 14:37:37 +0530

    Delete part_2 directory

--- part_2/phonebook/src/components/Notification.jsx
+++
@@ -1,8 +0,0 @@
-const Notification = ({ message, isError }) => {
-  if (!message) return null
-
-  const className = isError ? 'error' : 'note'
-  return <div className={className}>{message}</div>
-}
-
-export default Notification
